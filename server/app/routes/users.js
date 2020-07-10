3fa2873c3cf2a65ca1838d443e93a62a753ec0cb
hieuvu99 <star.hieu.99@gmail.com>
2020-07-10 23:26:06 +0700

    change doc enpoint from user to index

--- server/app/routes/users.js
+++ server/app/routes/users.js
@@ -2,13 +2,6 @@ var express = require("express");
 var router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = require('./apidoc');
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocs));
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
