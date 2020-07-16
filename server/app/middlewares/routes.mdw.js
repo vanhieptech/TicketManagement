d7d3c8733b1c45232c4db347e8d7261283d0662f
hieuvu99 <star.hieu.99@gmail.com>
2020-07-17 00:54:48 +0700

    add order route

--- server/app/middlewares/routes.mdw.js
+++ server/app/middlewares/routes.mdw.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
     app.use('/ticket', require('../routes/ticket.route'));
     app.use('/flight',require('../routes/flight.route'));
     app.use('/airport',require('../routes/airport.route'));
+    app.user('/order',require('../routes/order.route'));
 };
