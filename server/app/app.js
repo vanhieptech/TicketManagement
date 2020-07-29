a54ff5d206a4b570a794a652ed396530600d2e7b
Van Hiep <1712430@student.hcmus.edu.vn>
2020-07-30 00:24:16 +0700

    add dotenv

--- server/app/app.js
+++ server/app/app.js
@@ -1,63 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 var app = express();
+dotenv.config();
 
 //Middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-
-
-require('./middlewares/routes.mdw')(app);
-
+require("./middlewares/routes.mdw")(app);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // // set locals, only providing error in development
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-  res.status(err.status || 500);
-  res.json({
-    error: err.message
-  });
+    // // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
+    res.status(err.status || 500);
+    res.json({
+        error: err.message,
+    });
 });
 
 app.listen(process.env.port || 4000, () => {
-  console.log(`Server running at http://localhost:4000`);
-})
-
+    console.log(`Server running at http://localhost:4000`);
+});
 
 //Connect to mongodb
 mongoose
-  .connect('mongodb+srv://usermongodb:' + process.env.MONGO_ATLAS_PW + '@ticketmanagement.kto9n.mongodb.net/ticketmanangement?retryWrites=true&w=majority',
-    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-  .catch(err => console.log(err));
+    .connect(process.env.DB_CONNECTION, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    })
+    .catch((err) => console.log(err));
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Connect to db successfully')
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connect to db successfully");
 });
 module.exports = app;
