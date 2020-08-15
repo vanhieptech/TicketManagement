174ed996bffec812fca1955a4ce43c5109e82ad1
Van Hiep <1712430@student.hcmus.edu.vn>
2020-08-15 14:00:01 +0700

    add auth server

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,30 +1,34 @@
-const monggose = require('mongoose');
+const monggose = require("mongoose");
 const Schema = monggose.Schema;
-const moment = require('moment');
+const moment = require("moment");
+const bcryptjs = require("bcryptjs");
 
 const UserSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Name is required'],
+        required: [true, "Name is required"],
         //Tối thiểu phải 6 ký tự
-        minlength: [6, 'Name must have at least 6 character']
+        minlength: [6, "Name must have at least 6 character"],
     },
     phone: {
         type: String,
-        required: [true, 'Phone is required'],
+        // required: [true, "Phone is required"],
         //Tối thiểu phải 10 số
-        minlength: [10, 'Phone must have at least 10 numbers']
+        // minlength: [10, "Phone must have at least 10 numbers"],
     },
     email: {
         type: String,
-        required: [true, 'Email is required'],
+        required: [true, "Email is required"],
         //Match regex email
-        match: [/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, 'Email address invalid']
+        match: [
+            /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+            "Email address invalid",
+        ],
     },
     dob: {
         type: String,
-        required: [true, 'Birth day is required'],
-        minlength: [10, 'DOB must have at least 10 character']
+        // required: [true, "Birth day is required"],
+        // minlength: [10, "DOB must have at least 10 character"],
         // Nhỏ nhất phải 18 tuổi
         // max: function () {
         //     var eightyYearsAgo = new Date();
@@ -35,16 +39,16 @@ const UserSchema = new Schema({
     },
     gender: {
         type: String,
-        required: [true, 'Gender is required']
+        // required: [true, "Gender is required"],
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
-        minlength: [8, 'Password must have at least 8 character']
+        required: [true, "Password is required"],
+        minlength: [8, "Password must have at least 8 character"],
     },
     permission: {
-        type: 'String',
-        default: 'ROLE_USER'
+        type: "String",
+        default: "ROLE_USER",
     },
     createdDate: {
         type: Date,
@@ -53,8 +57,8 @@ const UserSchema = new Schema({
     updatedDate: {
         type: Date,
         // default: moment.utc(Date.now(),'DD-MM-YYYY HH:mm:ss')
-    }
+    },
 });
 
-const User = monggose.model('user', UserSchema);
+const User = monggose.model("user", UserSchema);
 module.exports = User;
